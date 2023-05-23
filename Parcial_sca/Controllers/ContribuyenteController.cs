702df61789f721e72a03710bf18c2987b629df96
WsPrj <wernisawruersp@gmail.com>
2023-05-22 23:09:50 -0300

    Add project files.

---
+++ Parcial_sca/Controllers/ContribuyenteController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Parcial_sca.Models;
+
+namespace Parcial_sca.Controllers
+{
+    public class ContribuyenteController : Controller
+    {
+        public static List<Contribuyente> listaContribuyentes = new List<Contribuyente>();
+
+        public IActionResult Index()
+        {
+            return View(listaContribuyentes);
+        }
+
+        public IActionResult AddC()
+        {
+            return View();
+        }
+    }
+}
---
+++ Parcial_sca/Models/Contribuyente.cs
@@ -0,0 +1,12 @@
+﻿namespace Parcial_sca.Models
+{
+    public class Contribuyente
+    {
+        public string CIva { get; set; }
+        public string Cuit { get; set; }
+        public string RZ { get; set; }
+        public string Domicilio { get; set; }
+        public string Email { get; set; }
+        public string CVenta { get; set; }
+    }
+}
